1c593d2f39f6fa16a1b10151a93be20baa4fb9b9
MuhammadNurFahmi <45891167+MuhammadNurFahmi@users.noreply.github.com>
2018-12-17 09:56:20 +0700

    Add files via upload

---
+++ konversi mata uang 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main ()
+
+{
+	int Dollar;
+	cout<<"Masukkan Nilai Dollar =";
+	cin>>Dollar;
+	float Rupiah;
+	Rupiah= Dollar*15000;
+	cout<<"Hasil Pertukaran = "<<Rupiah<<endl;
+	return 0;
+}
